937bcfb636e1c03214797793ada0d9aaf8ff5150
Prabhjeet Bissora <C21412282@mytudublin.ie>
2023-12-08 19:37:55 +0000

    created view class, renamed schedule class to modify class

---
+++ app/src/main/java/com/example/msd_assignment/ViewClass.java
@@ -0,0 +1,47 @@
+package com.example.msd_assignment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.msd_assignment.databinding.ModifyClassBinding;
+
+public class ViewClass extends Fragment {
+
+    private ModifyClassBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = ModifyClassBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.scheduleClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ViewClass.this)
+                        .navigate(R.id.action_ScheduleClass_to_AddClass);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
