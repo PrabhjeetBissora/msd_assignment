8699177a4cee3cb18b589184594293c59fe91ff3
Prabhjeet Bissora <C21412282@mytudublin.ie>
2023-12-12 07:46:22 +0000

    finish ClassApplication.javapplication

--- app/src/main/java/com/example/msd_assignment/ClassApplication.java
+++ app/src/main/java/com/example/msd_assignment/ClassApplication.java
@@ -2,5 +2,22 @@ package com.example.msd_assignment;
 
 import android.app.Application;
 
+import androidx.room.Room;
+
 public class ClassApplication extends Application {
+
+    // initialise database
+    public static ClassDatabase classDatabase;
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        // retrieved from: https://developer.android.com/reference/android/arch/persistence/room/Room
+        // build database
+        classDatabase = Room.databaseBuilder(getApplicationContext(),
+                ClassDatabase.class, "class-database")
+                .build();
+    }
+
 }
